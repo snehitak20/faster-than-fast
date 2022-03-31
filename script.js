548ffc6cf0456908c1250b6e7cbc70161722ed8d
Snehita Kolli <snehitakolli20@gmail.com>
2022-03-31 01:16:31 -0700

    almost final js file

--- script.js
+++ script.js
@@ -1,87 +1,70 @@
-// Variables for all character types 
-var lower = "abcdefghijklmnopqrstuvwxyz";
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var specials = "!\"#$%&'()*+,-./:;<=>?@[\]^{|}~";
-var chosen = "";
-
-
 // Assignment Code [GIVEN]
-// Add event listener to generate button [GIVEN]
-// Write password to the #password input [GIVEN]
 var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");
-var password = generatePassword();
-passwordText.value = password;
 
 
+// Arrays for all character types 
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1","2", "3", "4", "5", "6", "7", "8", "9"];
+var specials = ["!", "#", "$", "%", "&", "*", "+", "-", ".", "/", ";", "<", "=", ">", "?", "@", "^", "~"];
+var chosen = "";
 // Write password to the #password input [GIVEN]
 function writePassword() {
-  password.value === "";
-  var newPw = "";
-// Password criteria:
-  // Issue prompt to determine the length of password 
-  //To confirm types of characters used in the password   
-  var pwLength = prompt("Choose how long you would like your password to be (between 8-128 characters).");
-  var yesLower = confirm("Do you want lowercase letters in your password?");
-  var yesUpper = confirm("Do you want uppercase letters in your password?");
-  var yesNumbers = confirm("Do you want numbers in your password?");
-  var yesSpecials = confirm ("Do you want special characters in your password?");
-// For when character amounts <8 or >128
-// For when a input criterion is NOT selected--> use NaN 
-  if (pwLength < 8 || pwLength > 128) {
-    alert("Your password is not between 8-128 characters. Please try again.")
-    var pwLength = prompt("Choose how long you would like your password to be (between 8-128 characters).");
-  }
-// When NO character types are chosen 
-  else if (yesLower === false && yesUpper === false && yesNumbers === false && yesSpecials === false) {
-    alert("You must choose at least one password criteria.");
-    var yesLower = confirm("Do you want lowercase letters in your password?");
-    var yesUpper = confirm("Do you want uppercase letters in your password?");
-    var yesNumbers = confirm("Do you want numbers in your password?");
-    var yesSpecials = confirm ("Do you want special characters in your password?");
-  }
-// Generate the final password.
-  if (yesLower) {
-    chosen += lower;
-  }
-  if (yesUpper) {
-    chosen += upper;
-  }
-  if (yesNumbers) {
-    chosen += numbers;
-  }
-  if (yesSpecials) {
-    chosen += specials;
-  }
-  for (var i = 0; i < pwLength; i++) {
-    newPw += chosen.charAt(Math.floor(Math.random() * chosen.length));
-  }
-  password.value = randomString;
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 }
+// Add event listener to generate button [GIVEN]
+generateBtn.addEventListener("click", writePassword);
 
+// "generatePassword" is within the "writePassword" function, and will return to the final password
+function generatePassword() {
+// Issue prompt to determine the length of password 
+  var pwLength = prompt("Choose how long you would like your password to be (between 8-128 characters).");
+  // For when character amounts <8 or >128
+  // For when a input criterion is NOT selected--> use NaN 
+  if (pwLength < 8 || pwLength > 128) {
+    alert("Your password is not between 8-128 characters. Please try again.");
+    return generatePassword ()
+  }
+  if (isNaN(pwLength)) {
+    alert("You must choose a number.");
+    return generatePassword()
+  }
+//To confirm types of characters used in the password   
+  var yesLower = confirm("Click OK to confirm lowercase letters in your password?");
+  var yesUpper = confirm("Click OK to confirm uppercase letters in your password?");
+  var yesNumbers = confirm("Click OK to confirm in your password?");
+  var yesSpecials = confirm ("Click OK to confirm special characters in your password?");
+// When NO character types are chosen 
+  if (yesLower === false && yesUpper === false && yesNumbers === false && yesSpecials === false) {
+    alert("You must choose at least one password criteria.");
+    return generatePassword()
+  }
+//Store input from criteria choices
+var password = [];
+  if (yesLower) {
+    password = password.concat(lower);
+  }
 
+  if (yesUpper) {
+    password = password.concat(upper);
+  }
 
+  if (yesNumbers) {
+    password = password.concat(numbers);
+  }
 
+  if (yesSpecials) {
+    password = password.concat(specials);
+  }
 
-
-
+  console.log(password);
   
-
-//To confirm types of characters used in the password
-
-
-
-// Generates the final password
-
-
-
-
-
-
-
-
-
-
-// Use for loop and Math to help generate the RANDOMNESS
-
+// Generate the final password.
+for (var i = 0; i < pwLength; i++) {
+  chosen = chosen + password[Math.floor(Math.random() * password.length)];
+  console.log(chosen);
+}
+return chosen;
+}
